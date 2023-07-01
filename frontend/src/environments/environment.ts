734f140a00ce855e7a975710b56ecbd423216b7f
mitch-avis <mitchavis@gmail.com>
2023-06-30 18:35:41 -0600

    fix auth0 url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'https://dev-5txjjo1g1jpy427b.us.auth0.com/', // the auth0 domain prefix
+    url: 'dev-5txjjo1g1jpy427b.us', // the auth0 domain prefix
     audience: 'drinks', // the audience set for the auth0 app
     clientId: 'X7NhjMZpZUBWOGN2zI1oHMhkuG8d7FfL', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
